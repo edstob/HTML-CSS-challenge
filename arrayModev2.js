0e8ee4c6c01f98bd3027ed24c3a2955d27038fc0
edstob <51953407+edstob@users.noreply.github.com>
2019-07-15 23:21:40 +0200

    Update arrayModev2.js
    Added comments

--- arrayModev2.js
+++ arrayModev2.js
@@ -2,34 +2,33 @@ function arrayMode(array){
   var sortedArray = array.sort();
   var refinedSet = new Set(array);
   var refinedArray = [...refinedSet];
-  var modeArray = new Array(refinedArray.length);
-  var modeArrayVal = 0;
+  var modeArray = new Array(refinedArray.length); //array will keep the count of each number that appears in the array
+  var modeArrayVal = 0; //counter for modeArray
   var firstVal = 1;
   var lastVal = 1;
   
-  var e = 0;
-  
   for (var i = 0; i < sortedArray.length; i++) {
-    if (sortedArray[i] === sortedArray[i+1]) {
-      lastVal++;
+    if (sortedArray[i] === sortedArray[i+1]) { //comparing the two values
+      lastVal++; //the number of values in between the first and last time it shows up increases
     }
     else{
-      var total = lastVal - firstVal +1;
-      modeArray[modeArrayVal] = total;
+      var total = lastVal - firstVal +1; //find the difference between the two points = how many times it appears
+      modeArray[modeArrayVal] = total; //assign to modeArray
       modeArrayVal++
       
-      firstVal, lastVal = 1;
+      firstVal, lastVal = 1; //reset
     }
     
   }
   
-  var max = Math.max(...modeArray);
-  var maxIndex = modeArray.indexOf(max);
-  var mode = refinedArray[maxIndex];
+  var max = Math.max(...modeArray); //putting array into Math function
+  var maxIndex = modeArray.indexOf(max); //getting the index of the mode
+  var mode = refinedArray[maxIndex]; //refinedArray and modeArray have the same number of spaces in memory, 
+  //the index will connect the two. refinedArray will hold the value and modeArray will hold how many times it appears.
   
-  //return mode;
+  return mode;
   
-  console.log(sortedArray + "\n" + refinedArray + "\n" + modeArray + "\n" + "The mode is: " + mode);
+  //console.log(sortedArray + "\n" + refinedArray + "\n" + modeArray + "\n" + "The mode is: " + mode);
 }
 
 var a = [1, 3, 3, 3, 2];
